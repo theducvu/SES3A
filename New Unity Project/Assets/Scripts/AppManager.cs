07673fd8b3e8f2f13f0f22538ef6733274ab00f6
theducvu <vutheduc96@gmail.com>
2020-06-12 14:37:07 +1000

    Integrated UI into main scene

---
+++ New Unity Project/Assets/Scripts/AppManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Android;
+
+public class AppManager : MonoBehaviour
+{
+    public CanvasRenderer sliderCanvasRenderer;
+
+    public void Start() {
+        FixElements();
+        // Permission.RequestUserPermission(Permission.Camera);
+    }
+
+    void FixElements() {
+        if (sliderCanvasRenderer)
+            sliderCanvasRenderer.SetAlpha(0);
+    }
+
+    public void RequestLocationPermissions() {
+        // Permission.RequestUserPermission(Permission.FineLocation);
+        print("Requesting Location Permission");
+    }
+
+    public void GoToARScene() {
+        SceneManager.LoadScene("WorldScaleAR");
+    }
+
+    public void Recalibrate() {
+        print("Recalibrating Map..");
+    }
+}
---
+++ New Unity Project/Assets/Scripts/UI/SidePanelBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SidePanelBehaviour : MonoBehaviour
+{
+    RectTransform rect;
+    bool opened;
+    public Vector2 openedPos;
+    public Vector2 closedPos;
+    [Range(0f,1f)]
+    public float LerpSpeed = 0.5f;
+    
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    public void TogglePanel (bool open){
+        opened = open;
+    }
+
+    void Update()
+    {
+        if (opened) {
+            rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, openedPos, LerpSpeed);
+        } else {
+            rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, closedPos, LerpSpeed);
+        }
+    }
+}
